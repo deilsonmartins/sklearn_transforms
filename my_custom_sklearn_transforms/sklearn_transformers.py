d05eb54d42d727c1240ac4c123025c53116d80a9
Deilson Martins <45822038+deilsonmartins@users.noreply.github.com>
2020-08-17 14:56:18 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -10,26 +10,28 @@ class DropColumns(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
         return self
 
-    def transform(self, X):
+    def transform(self, X, y):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
-        return data.drop(labels=self.columns, axis='columns')
+        new_x = data.drop(labels=self.columns, axis='columns')
+        
+        return new_x, y
 
 class ADDColumns(BaseEstimator, TransformerMixin):
 
     def fit(self, X, y=None):
         return self
 
-    def transform(self, X):
+    def transform(self, X, y):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
 
         # Criação da nova coluna
         data['H_AULA_PRES/FALTAS'] = round(data['H_AULA_PRES'] / data['FALTAS'], 3)
-
+        
         # Retornamos um novo dataframe 
-        return data
+        return data, y
  
 class Balanceamento(BaseEstimator, TransformerMixin):
   
