0dfbcd8d5c9769c3f2ec1b6dd3e8f7121bd5d97b
Deilson Martins <45822038+deilsonmartins@users.noreply.github.com>
2020-08-17 15:18:14 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,31 +1,28 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 
-from imblearn.over_sampling import SMOTE
-
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
         self.columns = columns
 
-    def fit(self, X):
+    def fit(self, X, y=None):
         return self
 
     def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
-        x, y = X
-        data = x.copy()
+        data = X.copy()
         
         # Retornamos um novo dataframe sem as colunas indesejadas
         new_x = data.drop(labels=self.columns, axis='columns')
         
-        return new_x, y
+        return new_x
 
 class ADDColumns(BaseEstimator, TransformerMixin):
 
     def fit(self, X, y=None):
         return self
 
-    def transform(self, X, y=None):
+    def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
 
@@ -33,21 +30,6 @@ class ADDColumns(BaseEstimator, TransformerMixin):
         data['H_AULA_PRES/FALTAS'] = round(data['H_AULA_PRES'] / data['FALTAS'], 3)
         
         # Retornamos um novo dataframe 
-        return data, y
+        return data
  
-class Balanceamento(BaseEstimator, TransformerMixin):
-  
-    def fit(self, X):
-        return self
-    
-    def transform(self, X):
-       
-        x, y = X
-        # Instanciando o SMOTE
-        nr = SMOTE()
-        
-        # Pegando os dados balanceados
-        new_X, new_y = nr.fit_sample(x, y)
 
-        # Retornamos um novo dataframe
-        return new_X, new_y
